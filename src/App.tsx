8a3b48ca6cc632b86877c6aeb6c9e4059d054d14
Dan Galera <danerick.galera@gmail.com>
2021-01-13 22:58:32 +0800

    setup folder structure, added react-router-dom dependencies, initialized Home and Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import HomePage from "pages/HomePage";
+import LoginPage from "pages/LoginPage";
 
 function App() {
-  return <p>FEW: Mini Project</p>;
+  return (
+    <>
+      <Router>
+        <div className="container-fluid pt-3">
+          <Switch>
+            <Route exact path="/home" component={HomePage} />
+            <Route path="/login" component={LoginPage} />
+            <Redirect from="/" to="/login" />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
